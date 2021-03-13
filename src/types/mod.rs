d7031544feae5bb618aa9415c58f862356c0be4a
Horhik <horhik@tuta.io>
2021-03-13 11:23:40 +0300

    impl some sqlite types for SSK type

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,17 +2,38 @@ pub mod traits;
 use regex::Regex;
 use traits::*;
 
+use rusqlite::types::ToSqlOutput;
+use rusqlite::{Result, ToSql, types::{FromSql, ValueRef, FromSqlResult, FromSqlError}};
+
 #[derive(Debug, PartialEq)]
 pub struct SSK {
     pub sign_key: String,
     pub decrypt_key: String,
     pub settings: Option<String>,
 }
+
+impl ToSql for SSK {
+    fn to_sql(&self) -> Result<ToSqlOutput<'_>> {
+        Ok(ToSqlOutput::from(self.convert()))
+    }
+}
+
+impl FromSql for SSK{
+    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self>{
+        match SSK::parse(value.as_str()?) {
+            Some(res) => Ok(res),
+            None => Err(FromSqlError::InvalidType)
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct USK {
     pub ssk: SSK,
     pub index: i32,
 }
+
+
 
 #[derive(Debug, PartialEq)]
 pub struct SSKKeypair {
